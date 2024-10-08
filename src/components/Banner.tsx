b9f8b9914c4bf4c837e648974c3f6a399713d091
thubten1234 <thubten219@gmail.com>
2024-10-09 03:34:26 +1100

    c

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Component.css";
 import "./App.css";
 
@@ -8,13 +7,11 @@ function Banner() {
       <video className="banner-video" autoPlay loop muted playsInline>
         <source src="DepsiResort.mp4" type="video/mp4" />
       </video>
+      <div className="tinted-overlay"></div> {/* Moved this div outside */}
       <div className="video-overlay">
         <h1>Depsi Resort</h1>
         <p>A Tranquil Fusion of Tradition and Modernity</p>
-        <div>
-          <div className="tinted-overlay"></div>
-          <button className="booking-btn">Book Now</button>
-        </div>
+        <button className="booking-btn">Book Now</button>
       </div>
     </div>
   );
