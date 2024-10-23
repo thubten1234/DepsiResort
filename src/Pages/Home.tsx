3ce2d97adda72ccc752141efd61814badc2a1ac2
thubten1234 <thubten219@gmail.com>
2024-10-23 16:02:58 +1100

    commit

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState, useRef } from "react";
 import Banner from "../components/Banner";
 import StoryLine from "../components/StoryLine";
 import Activities from "../components/Activities";
@@ -8,6 +9,47 @@ import Room from "../components/Rooms";
 import Navbar from "../components/Navbar";
 
 function Home() {
+  const roomRef = useRef(null);
+  const activitiesRef = useRef(null);
+  const facilitiesRef = useRef(null);
+
+  // State to track visibility of each component
+  const [isVisible, setIsVisible] = useState({
+    room: false,
+    activities: false,
+    facilities: false,
+  });
+
+  // Function to check if an element is in the viewport
+  const isInViewport = (
+    element: { getBoundingClientRect: () => any } | null
+  ) => {
+    if (!element) return false;
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+  };
+
+  // Function to handle scroll event
+  const handleScroll = () => {
+    if (window.innerWidth <= 768) {
+      setIsVisible((prevVisibility) => ({
+        room: prevVisibility.room || isInViewport(roomRef.current),
+        activities:
+          prevVisibility.activities || isInViewport(activitiesRef.current),
+        facilities:
+          prevVisibility.facilities || isInViewport(facilitiesRef.current),
+      }));
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div>
       <div className="banner-container">
@@ -20,10 +62,25 @@ function Home() {
         </div>
         <div className="informationsec d-flex flex-column align-items-center">
           <div className="container">
-            <Room />
+            <div
+              ref={roomRef}
+              className={`slide-in ${isVisible.room ? "visible" : ""}`}
+            >
+              <Room />
+            </div>
             <br />
-            <Activities />
-            <Facilities />
+            <div
+              ref={activitiesRef}
+              className={`slide-in ${isVisible.activities ? "visible" : ""}`}
+            >
+              <Activities />
+            </div>
+            <div
+              ref={facilitiesRef}
+              className={`slide-in ${isVisible.facilities ? "visible" : ""}`}
+            >
+              <Facilities />
+            </div>
           </div>
         </div>
         <div>
